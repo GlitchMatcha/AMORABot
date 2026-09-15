8331dc703351cebfa1f8d99986e87aed54f760bd
GlitchyMatcha <mytea1108@gmail.com>
2026-09-16 04:16:46 +0700

    Report Generation Monthly Edge Case Fix

--- src/main/java/com/amore/CommandListener.java
+++ src/main/java/com/amore/CommandListener.java
@@ -3283,7 +3283,11 @@ public class CommandListener extends ListenerAdapter {
 
         if (componentId.equals("reopen_ticket")) {
             event.getMessage().delete().queue();
-            event.getChannel().sendMessage("🔓 **Ticket reopened by " + event.getUser().getAsMention() + "!**").queue();
+            event.getChannel().sendMessage("🔓 **Ticket reopened by " + event.getUser().getAsMention() + "!**\n*The ticket controls have been restored below:*")
+                 .addActionRow(
+                     Button.primary("ping_hr", " Ping HR Team"),
+                     Button.danger("initiate_close_ticket", "🔒 Close Ticket")
+                 ).queue();
             return;
         }
 
