21cb2423f62a67fd013a9d07178c4e60acb51367
GlitchyMatcha <mytea1108@gmail.com>
2026-09-16 16:51:03 +0700

    Role Pickup Button Recoloring

--- src/main/java/com/amore/CommandListener.java
+++ src/main/java/com/amore/CommandListener.java
@@ -1494,24 +1494,24 @@ public class CommandListener extends ListenerAdapter {
                 "⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣⌣";
 
             EmbedBuilder panelEmbed = new EmbedBuilder()
-                .setColor(Color.decode("#FF5FA2")) // 
+                .setColor(Color.decode("#FF5FA2"))
                 .setDescription(aestheticPanel);
 
             event.getChannel().sendMessageEmbeds(panelEmbed.build())
                 .addActionRow(
-                    Button.secondary("role_member", "꒰ ⟰  ۫   ִ  ᴏꜰꜰɪᴄɪᴀʟ ᴍᴇᴍʙᴇ𝐫  ִ   ۫  ⟰ ꒱ ")
+                    Button.secondary("role_member", "꒰ ⟰  ۫   ִ  ᴏꜰꜰɪᴄɪᴀʟ ᴍ𝐞ᴍ𝐛𝐞𝐫  ִ   ۫  ⟰ ꒱ ")
                           .withEmoji(net.dv8tion.jda.api.entities.emoji.Emoji.fromFormatted("<a:8_heart:1514920927178264626>")),
-                    Button.secondary("role_visitor", "꒰  ᴥ  ۫   ִ  ᴀᴍᴏʀᴀ ᴠɪꜱɪᴛᴏʀ  ִ   ۫  ᴥ  ꒱ ")
+                    Button.danger("role_visitor", "꒰  ᴥ  ۫   ִ  ᴀᴍᴏʀᴀ ᴠɪꜱɪᴛ𝐨ʀ  ִ   ۫  ᴥ  ꒱ ") 
                           .withEmoji(net.dv8tion.jda.api.entities.emoji.Emoji.fromFormatted("<a:8_music:1514920332006264953>"))
                 )
                 .addActionRow(
                     Button.secondary("role_seller", "꒰ ര  ۫   ִ  ᴀ𝚙𝚙𝚕𝚢 ꜰᴏʀ ꜱᴇ𝚕𝚕𝚎𝚛  ִ   ۫  ര ꒱ ")
                           .withEmoji(net.dv8tion.jda.api.entities.emoji.Emoji.fromFormatted("<a:8_sparkle:1514920890264059954>")),
-                    Button.secondary("role_positions", "꒰ ꧞  ۫   ִ  ᴀᴍᴏʀᴀ ᴘᴏꜱɪᴛɪᴏɴꜱ  ִ   ۫  ꧞ ꒱")
+                    Button.danger("role_positions", "꒰ ꧞  ۫   ִ  ᴀᴍᴏʀᴀ ᴘᴏꜱɪᴛɪ0ɴꜱ  ִ   ۫  ꧞ ꒱") 
                           .withEmoji(net.dv8tion.jda.api.entities.emoji.Emoji.fromFormatted("<a:5_flower:1514918368346636288>"))
                 ).queue();
 
-            event.reply(" Beautiful Welcome Panel deployed!").setEphemeral(true).queue();
+            event.reply(" Updated Gateway Role Panel ").setEphemeral(true).queue();
             return;
         }
         if (event.getName().equals("eventsetup")) {
