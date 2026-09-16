8116fc1c7b81c73e8108cebf94127ced773b0f6e
GlitchyMatcha <mytea1108@gmail.com>
2026-09-16 15:33:45 +0700

    Role Pickup System Fix

--- src/main/java/com/amore/CommandListener.java
+++ src/main/java/com/amore/CommandListener.java
@@ -3361,8 +3361,8 @@ public class CommandListener extends ListenerAdapter {
 
             event.getChannel().sendMessage("🔓 **Ticket reopened by " + event.getUser().getAsMention() + "!**\n*Ticket controls have been restored below:*")
                  .addActionRow(
-                     Button.primary("ping_hr", "🔔 Ping HR Team"),
-                     Button.success("claim_ticket", "✋ Claim Ticket"), 
+                     Button.primary("ping_hr", " Ping HR Team"),
+                     Button.success("claim_ticket", " Claim Ticket"), 
                      Button.danger("initiate_close_ticket", "🔒 Close Ticket")
                  ).queue();
             return;
